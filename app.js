b7f11f9212968097f97aee2775d5409f274fcca5
iamshadmirza <md.shad1@yahoo.com>
2019-03-12 01:20:52 +0530

    basics over, boilerplate setup

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 console.log('Starting notepad app');
+
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes');
-console.log(notes.addNote());
+
 const _ = require('lodash');
-var filteredArray = _.uniq(['Shad', 1, 2, 3, 'Shad', 2, 3, 4]);
-console.log(filteredArray);
-// var user = os.userInfo();
-// console.log('Hello', user.username);
-// fs.appendFile('greetings.txt', `\nHello ${user.username} ` + `You are ${notes.age} years old`, (error) => {
-//     console.log('error', error);
-// });
