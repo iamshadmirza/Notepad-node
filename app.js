6c5c6dd3e5b1ae3a28c62362bdffbdeb21226aaf
iamshadmirza <md.shad1@yahoo.com>
2019-03-14 06:32:27 +0530

    playig with yargs

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const fs = require('fs');
 const notes = require('./notes');
 
 const _ = require('lodash');
+const yargs = require('yargs');
+
+var argv = yargs.argv;
+console.log('Yargs', argv);
+
+if (argv._[0] === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (argv._[0] === 'read') {
+    notes.getNote(argv.title);
+} else if (argv._[0] === 'remove') {
+    notes.removeNote(argv.title);
+} else if (argv._[0] === 'list') {
+    notes.getAll();
+}
--- notes.js
+++ notes.js
@@ -1,4 +1,21 @@
 console.log('Starting notes.js');
-module.exports.addNote = () => {
-    return 'Add Note Function';
+
+var addNote = (title, body) => {
+    console.log('Adding Note', title, body);
+};
+
+var getAll = () => {
+    console.log('getting all');
+};
+
+var getNote = (title) => {
+    console.log('reading', title);
 }
+
+var removeNote = (title) => {
+    console.log('removing', title);
+}
+
+module.exports = {
+    addNote, getAll, getNote, removeNote
+};
